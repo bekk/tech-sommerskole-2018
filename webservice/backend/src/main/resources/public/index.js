535c2a3363982453d47380db06e17cce42111044
Øyvind Skaar <oyvind.skaar@bekk.no>
2018-06-19 17:33:27 +0200

    Filter abv

--- webservice/backend/src/main/resources/public/index.js
+++ webservice/backend/src/main/resources/public/index.js
@@ -1,4 +1,4 @@
-function createLogger(selector){
+function setupLogger(selector) {
     const error_console = document.querySelector(selector);
     return txt => {
         if (error_console) {
@@ -8,67 +8,76 @@ function createLogger(selector){
     };
 }
 
-async function fetchBeers(props){
-    const url = new URL('/beer', document.location.href);
-    if (props.searchParams) {
-        Object.entries(props.searchParams).forEach(entry => {
-            url.searchParams.append(entry[0], entry[1]);
-        })
-    }
-    try {
-        const response = await fetch(url.href);
-        if(!response.ok){
-            props.error(`Fetching ${url} returned ${response.status}`);
-            return;
-        }
-        return response.json();
-    }
-    catch (e) {
-        props.errorLog(e);
+function setUpFetcher({errorLog}) {
+    const path = '/beer';
+    let searchState = {};
+    return function (params) {
+        const {searchParams} = params || {};
+        const url = new URL(path, document.location.href);
+        searchState = Object.assign(searchState, searchParams);
+        Object.entries(searchState)
+            .filter(entry => typeof entry[1] !== 'undefined' && entry[1] !== '')
+            .forEach(entry => {
+                url.searchParams.append(entry[0], entry[1]);
+            });
+        return fetch(url.href)
+            .then(response => {
+                if (!response.ok) {
+                    errorLog(`Fetching ${url} returned ${response.status}`);
+                    return false;
+                }
+                return response.json();
+            })
+            .catch(error => errorLog(error));
     }
 }
 
-function renderBeerTable(tableSelector, beers) {
-    if (!beers) return;
+function setupRenderer(tableSelector) {
     const table = document.querySelector(tableSelector);
-    table.innerHTML = '';
-    const headerRow = document.createElement("tr");
-    const buildCell = (txt, tag = 'td') => {
-        const node = document.createElement(tag);
-        node.innerText = txt || '';
-        return node;
-    };
-    ['Name', 'Brewery', 'ABV', 'Independent', 'Country'].forEach(hdr => {
-        headerRow.appendChild(buildCell(hdr, 'th'));
-    });
-    table.appendChild(headerRow);
-    beers.forEach(beer => {
-        const brewery = beer.brewery || {};
-        const country = beer.country || {};
-        const row = document.createElement('tr');
-        row.appendChild(buildCell(beer.name));
-        row.appendChild(buildCell(brewery.name));
-        row.appendChild(buildCell(beer.abv));
-        row.appendChild(buildCell(beer.independent ? 'Independent' : 'Corporate'));
-        row.appendChild(buildCell(country.name));
-        table.appendChild(row);
-    })
+    return function (beers) {
+        if (!beers) return;
+        table.innerHTML = '';
+        const headerRow = document.createElement("tr");
+        const buildCell = (txt, tag = 'td') => {
+            const node = document.createElement(tag);
+            node.innerText = txt || '';
+            return node;
+        };
+        ['Name', 'Brewery', 'ABV', 'Country'].forEach(hdr => {
+            headerRow.appendChild(buildCell(hdr, 'th'));
+        });
+        table.appendChild(headerRow);
+        beers.forEach(beer => {
+            const brewery = beer.brewery || {};
+            const country = beer.country || {};
+            const row = document.createElement('tr');
+            row.appendChild(buildCell(beer.name));
+            row.appendChild(buildCell(brewery.name));
+            row.appendChild(buildCell(beer.abv));
+            row.appendChild(buildCell(country.name));
+            table.appendChild(row);
+        })
+    }
 }
 
 function setUpFilters(props) {
-    const {errorLog, mainTableSelector} = props;
-    const indieCheckbox = document.querySelector('#filter_indie');
-    indieCheckbox.onchange = event => {
-        const checked = event.target.checked;
-        const searchParams = checked ? {indie: checked} : {};
-        fetchBeers({errorLog, searchParams})
-            .then(beers => renderBeerTable(mainTableSelector, beers));
-    }
+    const {renderBeers, getBeers} = props;
+    const setupTextBoxFilter = (selector, name) => {
+        const box = document.querySelector(selector);
+        box.onchange = event => {
+            getBeers({searchParams: {[name]: event.target.value}})
+                .then(beers => renderBeers(beers));
+        }
+    };
+    setupTextBoxFilter('#filter_abv_min', 'minAbv');
+    setupTextBoxFilter('#filter_abv_max', 'maxAbv');
 }
 
 async function init(mainTableSelector, errorConsoleSelector) {
-    const errorLog = createLogger(errorConsoleSelector);
-    const beers = await fetchBeers({errorLog});
-    renderBeerTable(mainTableSelector, beers);
-    setUpFilters({errorLog, mainTableSelector});
+    const errorLog = setupLogger(errorConsoleSelector);
+    const getBeers = setUpFetcher({errorLog});
+    const renderBeers = setupRenderer(mainTableSelector);
+    const beers = await getBeers();
+    renderBeers(beers);
+    setUpFilters({errorLog, renderBeers, getBeers});
 }
