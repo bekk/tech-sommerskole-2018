1339c1a9b3b5e00a14a16f82394671328af70517
Øyvind Skaar <oyvind.skaar@bekk.no>
2018-06-19 13:54:56 +0200

    Rendering table

--- webservice/backend/src/main/resources/public/index.js
+++ webservice/backend/src/main/resources/public/index.js
@@ -13,7 +13,8 @@ async function fetchBeers(props){
     try {
         const response = await fetch(url);
         if(!response.ok){
-            throw new Error(`Fetching ${url} returned ${response.status}`);
+            props.error(`Fetching ${url} returned ${response.status}`);
+            return;
         }
         return response.json();
     }
@@ -22,12 +23,36 @@ async function fetchBeers(props){
     }
 }
 
+function renderBeerTable(tableSelector, beers) {
+    if (!beers) return;
+    const table = document.querySelector(tableSelector);
+    table.innerHTML = '';
+    const headerRow = document.createElement("tr");
+    const buildCell = (txt, tag = 'td') => {
+        const node = document.createElement(tag);
+        node.innerText = txt || '';
+        return node;
+    };
+    ['Name', 'Brewery', 'ABV', 'Independent', 'Country'].forEach(hdr => {
+        headerRow.appendChild(buildCell(hdr, 'th'));
+    });
+    table.appendChild(headerRow);
+    beers.forEach(beer => {
+        const brewery = beer.brewery || {};
+        const country = beer.country || {};
+        const row = document.createElement('tr');
+        row.appendChild(buildCell(beer.name));
+        row.appendChild(buildCell(brewery.name));
+        row.appendChild(buildCell(beer.abv));
+        row.appendChild(buildCell(beer.independent ? 'Independent' : 'Corporate'));
+        row.appendChild(buildCell(country.name));
+        table.appendChild(row);
+    })
+}
+
 async function init(mainTableSelector, errorConsoleSelector) {
     const errorLog = createLogger(errorConsoleSelector);
     const beers = await fetchBeers({errorLog});
-    if (beers) {
-        const table = document.querySelector(mainTableSelector);
-        table.innerText = beers;
-    }
+    renderBeerTable(mainTableSelector, beers);
 }
 
