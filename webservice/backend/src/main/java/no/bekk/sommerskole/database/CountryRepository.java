38aa65f8f4bbcf7f9a6357f29763e82bff6562e6
Jørund Amsen <jorund.amsen@bekk.no>
2018-07-24 15:57:05 +0200

    Remove country repository

--- webservice/backend/src/main/java/no/bekk/sommerskole/database/CountryRepository.java
+++ webservice/backend/src/main/java/no/bekk/sommerskole/database/CountryRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import javax.inject.Inject;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
@@ -20,30 +21,8 @@ public class CountryRepository {
     }
 
     public List<Country> getCountries() {
-        String query = new SelectBuilder()
-                .column("countries.code")
-                .column("countries.title")
-                .column("countries.key")
-                .column("continents.title AS continent")
-                .column("COUNT(beers.id) AS beerCount")
-                .from("countries")
-                .leftJoin("continents ON countries.continent_id = continents.id")
-                .leftJoin("beers ON countries.id = beers.country_id")
-                .groupBy("countries.code")
-                .groupBy("countries.title")
-                .groupBy("countries.title")
-                .orderBy("countries.title")
-                .toString();
-
-        return jdbc.query(query, CountryRepository::mapToCountry);
+        return Collections.emptyList();
     }
 
-    private static Country mapToCountry(ResultSet rs, int rowNum) throws SQLException {
-        return new Country()
-                .setCountryCode(rs.getString("code"))
-                .setKey(rs.getString("key"))
-                .setName(rs.getString("title"))
-                .setContinent(rs.getString("continent"))
-                .setNumberOfBeers(rs.getInt("beerCount"));
-    }
+
 }
