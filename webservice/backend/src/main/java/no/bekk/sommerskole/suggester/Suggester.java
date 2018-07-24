6bdfe355daceb80d2ac0c6f622b4c667331098e1
Jørund Amsen <jorund.amsen@bekk.no>
2018-07-24 15:42:41 +0200

    Remove suggestion implementations

--- webservice/backend/src/main/java/no/bekk/sommerskole/suggester/Suggester.java
+++ webservice/backend/src/main/java/no/bekk/sommerskole/suggester/Suggester.java
@@ -2,17 +2,11 @@ package no.bekk.sommerskole.suggester;
 
 import no.bekk.sommerskole.database.BeerRepository;
 import no.bekk.sommerskole.domain.Beer;
-import no.bekk.sommerskole.domain.requirements.Requirement;
-import no.bekk.sommerskole.filter.BeerFilter;
+import no.bekk.sommerskole.domain.requirements.RequirementsForm;
 
 import javax.inject.Inject;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static java.util.Collections.reverseOrder;
-import static java.util.Map.Entry.comparingByValue;
-import static java.util.stream.Collectors.toList;
 
 public class Suggester {
 
@@ -23,23 +17,10 @@ public class Suggester {
         this.beerRepository = beerRepository;
     }
 
-    public List<Beer> suggestBeer(List<Requirement> requirements) {
+    public List<Beer> suggestBeer(RequirementsForm form) {
 
-        List<Beer> beers = beerRepository.getBeer(new BeerFilter().setLimit(9999));
-        return findTop10(requirements, beers);
+        return Collections.emptyList();
     }
 
-    private List<Beer> findTop10(List<Requirement> requirements, List<Beer> beers) {
-        Map<Beer, Double> beerWorth = beers.stream().collect(Collectors.toMap(beer -> beer, beer -> worthOfBeer(requirements, beer)));
 
-        return beerWorth.entrySet().stream()
-                .sorted(reverseOrder(comparingByValue()))
-                .limit(10)
-                .map(Map.Entry::getKey)
-                .collect(toList());
-    }
-
-    private Double worthOfBeer(List<Requirement> requirements, Beer beer) {
-        return requirements.stream().map(req -> req.calculateCost(beer) * req.getWeight()).reduce(0.0, Double::sum);
-    }
 }
